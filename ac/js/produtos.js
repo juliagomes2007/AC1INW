8e7884728afe8cc14880585f8d5e962a3988013b
Julia Gomes <98828706+juliagomes2007@users.noreply.github.com>
2023-05-29 11:58:38 -0300

    Add files via upload

---
+++ ac/js/produtos.js
@@ -0,0 +1,35 @@
+const produtos = [{
+    _id: 1,
+    produto: "Caneta Esferográfica",
+    preco: 2.50,
+    cor: "Azul",
+    marca: "Bic"
+  },
+  {
+    _id: 2,
+    produto: "Lápis de Cor",
+    preco: 6.00,
+    cor: "Multicolorido",
+    marca: "Faber-Castell"
+  },
+  {
+    _id: 3,
+    produto: "Caderno Universitário",
+    preco: 15.00,
+    cor: "Rosa",
+    marca: "Tilibra"
+  },
+  {
+    _id: 4,
+    produto: "Bloco de Notas",
+    preco: 3.50,
+    cor: "Amarelo",
+    marca: "3M"
+  },
+  {
+    _id: 5,
+    produto: "Corretivo Líquido",
+    preco: 4.50,
+    cor: "Branco",
+    marca: "Mercur"
+  }]
